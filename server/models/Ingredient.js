83462f0f6afa9377735b8f3604ea9959c752ef58
Bryant Lam <89410481+bryant-lam@users.noreply.github.com>
2024-12-29 16:12:57 -0800

    added ingredient model

---
+++ server/models/Ingredient.js
@@ -0,0 +1,41 @@
+import query from "../index.js";
+
+class Ingredient {
+    static async getById(id) {
+        const result = await db.query(
+            `SELECT * FROM Ingredients WHERE id = $1`,
+            [id]);
+        return result.rows[0];
+    }
+
+    static async getByName(name) {
+        const result = await db.query(
+            `SELECT * FROM Ingredients WHERE name = $1`,
+            [name]);
+        return result.rows[name];
+    }
+
+    static async create(name) {
+        const result = await query(
+            `INSERT INTO Ingredients (name) VALUES ($1) RETURNING *`,
+            [name]
+        );
+        return result.rows[0];
+    }
+
+    static async update(id, name) {
+        const result = await query(
+            `UPDATE Ingredients SET name = $1 WHERE id = $2 RETURNING *`,
+            [name, id]
+        );
+        return result.rows[0];
+    }
+
+    static async delete(id) {
+        const result = await query(
+            `DELETE FROM Ingredients WHERE id = $1 RETURNING *`,
+            [id]
+        );
+        return result.rows[0];
+    }
+}
